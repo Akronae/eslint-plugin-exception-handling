6287127a09f540424ebba6a5c8092e6329dce551
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-09-21 14:30:29 +0200

    fix: fixed test config

--- eslint.config.test.js
+++ eslint.config.test.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 import tseslint from "typescript-eslint";
+import { plugin as ex } from "./dist/index.mjs";
 import config from "./eslint.config.js";
 
 const ignores = [
@@ -12,4 +13,11 @@ const ignores = [
   "vitest.config.js",
 ];
 
-export default tseslint.config(...config.map((x) => ({ ...x, ignores })));
+export default tseslint.config(...config.map((x) => ({ ...x, ignores })), {
+  plugins: { ex },
+  rules: {
+    "ex/no-unhandled": "error",
+    "ex/might-throw": "warn",
+    "ex/use-error-cause": "warn",
+  },
+});
